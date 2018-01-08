6370f99ec48cc7e42b60d325f4a852f352f13627
Son Pham <sonphamfr2017@gmail.com>
2018-01-08 17:24:40 +0700

    con ga

---
+++ resources/views/header.blade.php
@@ -0,0 +1,156 @@
+<div class="top-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 top-link">
+                <ul>
+                    <li><i class="glyphicon glyphicon-earphone"></i></li>
+                    <li>Hotline : 0983807784</li>
+                </ul>
+            </div>
+            <div class="col-md-5 col-md-offset-3 top-menu hidden-xs hidden-sm">
+                <ul>
+                    <li>
+                        <a href="#"><i class="glyphicon glyphicon-pencil"></i>Kiểm tra đơn hàng</a>
+                    </li>
+                    <li><a href="#"><i class="glyphicon glyphicon-shopping-cart"></i>Giỏ hàng</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-log-in"></i>Đăng nhập</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-plus-sign"></i>Đăng ký</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="header-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 header-left">
+                <div class="logo">
+                    <a href="#"><img src="images/logo.png"></a>
+                </div>
+            </div>
+            <div class="col-md-9  header-right">
+                <div class="sale-policy clearfix hidden-sm hidden-xs">
+                    <ul class="clearfix">
+                        <li class="shipping">
+                            <i class="fa fa-truck"></i>
+                            <span>Giao hàng miễn phí</span>
+                        </li>
+                        <li class="payment">
+                            <i class="fa fa-money"></i>
+                            <span>Thanh toán linh hoạt</span>
+                        </li>
+                        <li>
+                            <i class="fa fa-refresh"></i>
+                            <span>Trả hàng trong 30 ngày</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cart">
+                    <a href="#">
+                        <i class="fa fa-shopping-bag" style="font-size: 36px;"></i>
+                        <span>    0 sp - 0đ </span>
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="search hidden-sm hidden-xs">
+                    <div class="form-search">
+                        <div class="form-search-controls">
+                            <input type="text" class="form-control" name="search" id="txtSearch" placeholder="Tìm kiếm">
+                        </div>
+                        <div class="select-categories">
+                            <select class="selectpicker">
+                                <option value="0" selected="selected">Tất cả</option>
+                                <option value="1">Thời trang nam</option>
+                                <option value="2">Thời trang nữ</option>
+                                <option value="3">Mỹ phẩm</option>
+                                <option value="4">Túi xách</option>
+                            </select>
+                        </div>
+                        <button class="btn btn-search" title="Search" type="button" id="btnSearch">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+{{--<div class="clearfix"></div>--}}
+<div class="navigation-menu clearfix">　
+    <div class="container container-fluid">
+        <div class="menu-top">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="menu-cate menu-quick-select">
+                        <div class="menu-cate-title">
+                            <span>
+                                <i class="glyphicon glyphicon-th-list"></i>
+                                <a href="#">Danh mục sản phẩm</a>
+                            </span>
+                        </div>
+                        {{--<ul class="menu-cate-content">--}}
+                            {{--<li>--}}
+                                {{--<a href="#">--}}
+                                    {{--<i>Thời trang nam</i>--}}
+                                    {{--<span class="arrow-menu"></span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="#">--}}
+                                    {{--<i>Thời trang nữ</i>--}}
+                                    {{--<span class="arrow-menu"></span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="#">--}}
+                                    {{--<i>Mỹ phẩm</i>--}}
+                                    {{--<span class="arrow-menu"></span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="#">--}}
+                                    {{--<i>Túi xách</i>--}}
+                                    {{--<span class="arrow-menu"></span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                        {{--</ul>--}}
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="navbar nav-menu">
+                        <div class="navbar-header">
+                            <div id="mobile-menu" class="mobile-menu collapse navbar-collapse">
+                                <ul class="menu nav navbar-nav">
+                                    <li class="level0">
+                                        <a href="#"><span>Trang chủ</span></a>
+                                    </li>
+                                    <li class="level0">
+                                        <a href="#">Sản phẩm</a>
+                                        {{--<ul class="level1">--}}
+                                            {{--<li>--}}
+                                                {{--<a href="#"><span></span></a>--}}
+                                            {{--</li>--}}
+                                        {{--</ul>--}}
+                                    </li>
+                                    <li class="level0">
+                                        <a href="#">Tin tức</a>
+                                    </li>
+                                    <li class="level0">
+                                        <a href="#">Giới thiệu</a>
+                                    </li>
+                                    <li class="level0">
+                                        <a href="#">Liên hệ</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/introduction.blade.php
@@ -0,0 +1,105 @@
+@extends('master')
+@section('content')
+<div class="introduction-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="menu-about">
+                    <h3>
+                        <span>Giới thiệu</span>
+                    </h3>
+                    <ul>
+                        <li>
+                            <a href="">Về chúng tôi</a>
+                        </li>
+                        <li>
+                            <a href="#">Lĩnh vực hoạt động</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="breadcrumb clearfix">
+                    <ul>
+                        <li class="home">
+                            <a href="#">Trang chủ</a>
+                        </li>
+                        <li class="icon-li">
+                                <strong>Giới thiệu</strong>
+                        </li>
+                    </ul>
+                </div>
+                <div class="about-detail">
+                    <div class="about-block">
+                        <h1>Về chúng tôi</h1>
+                        <div class="content">
+                            <p>
+                                <span>
+                                    Ra đời vào cuối năm 2011, SieuMua.Com đã sớm có một vị trí vững chắc trên thị trường thương mại điện tử trong nước, trở thành địa chỉ mua sắm online đầy tin cậy, tiện lợi cho khách hàng tại TP.HCM và các tỉnh thành khác trên toàn quốc.
+                                </span>
+                                <br><br>
+                                <span>
+                                    <strong>Tầm nhìn :</strong>
+                                </span>
+                                <br>
+                                <span>
+                                    SieuMua.Com  là một website thời trang online, phục vụ nhu cầu mua sắm trực tuyến bao gồm: chọn và mua hàng trực tuyến nhanh chóng, thanh toán trực tuyến an toàn, giao hàng tận nơi, chăm sóc và tư vấn thân thiện. Với mong muốn đem đến cho khách hàng những sản phẩm chất lượng,thời trang với giá cả hơp lý nhất, mang ứng dụng cao, hiện đại, năng động, phù hợp với văn hóa, gu thẩm mỹ của người Việt Nam.
+                                </span>
+                                <br><br>
+                                <span>
+                                    <strong>Sứ mệnh :</strong>
+                                </span>
+                                <br>
+                                <span>
+                                    Sản phẩm trên SieuMua.Com  là những sản phẩm của thời trang hiện đại  mang tính hữu dụng để mọi người có thể tự do kết hợp và sáng tạo chúng trong phong cách độc đáo riêng của từng người mà không phân biệt tuổi tác và ngành nghề. Mỗi sản phẩm được đăng trên website SieuMua.Com đều được tuyển chọn và bảo đảm chất lượng, và chúng tôi hi vọng đó cũng sẽ là món quà ý nghĩa để bạn giành cho những người thân yêu nhất.
+                                </span>
+                                <br><br>
+                                <span>
+                                    <strong>Sản phẩm chủ đạo của chúng tôi là : </strong>
+                                </span>
+                                <br>
+                                <span>
+                                    - Các sản phẩm thời trang nam nữ, bao gồm: Giày dép, quần áo, thể thao, phụ kiện, làm đẹp, váy-đầm... với mẫu mã đa dạng, đảm bảo phù hợp với thị hiếu thời trang và túi tiền của quý khách.
+                                </span>
+                                <br><br>
+                                <span>
+                                    Các sản phẩm của chúng tôi đều được nhập từ các nhà cung cấp thời trang uy tín,làm từ các chất liệu tốt, đáp ứng đầy đủ các yêu cầu về thẩm mỹ và chất lượng sản phẩm. Ngoài ra SieuMua.Com luôn cố gắng khai thác thêm nhiều mẫu mã, đảm bảo được sự đa dạng và độc đáo của mỗi sản phẩm khi xuất hiện trên website SieuMua.Com. Chúng tôi sẽ luôn đáp ứng nhằm phục vụ các Quý khách một cách tốt nhất.
+                                </span>
+                                <br><br>
+                                <span>
+                                    Đặt mục tiêu trở thành một trung tâm mua sắm trực tuyến hàng đầu tại Việt Nam về các sản phẩm dẫn đầu về uy tín, chất lượng và dịch vụ, Si
+                                </span>
+                                <br><br>
+                                <span>
+                                    - Cung cấp các sản phẩm - dịch vụ chất lượng tốt, giá cả phù hợp với túi tiền người tiêu dùng.
+                                </span>
+                                <br>
+                                <span>
+                                    - Các sản phẩm được kiểm tra, sàng lọc kỹ càng, đảm bảo an toàn cho sức khỏe cho người sử dụng.
+                                </span>
+                                <br>
+                                <span>
+                                    - Bảo hành nghiêm túc theo quy định của nhà sản xuất.
+                                </span>
+                                <br>
+                                <span>
+                                    - Được đổi, trả hàng trong vòng 15 ngày hoàn toàn miễn phí.
+                                </span>
+                                <br>
+                                <span>
+                                    - Tư vấn, hướng dẫn sử dụng với từng sản phẩm, đặc biệt với các sản phẩm chuyên dụng.
+                                </span>
+                                <br>
+                                <br><br>
+                                <span>
+                                    Với các tiêu chí về chất lượng và một đội ngũ năng động, nhiệt tình, giàu kinh nghiệm, SieuMua.Com nhất định sẽ là điểm mua sắm lý tưởng cho các đối tượng khách hàng trên cả nước, nhanh gọn, an toàn và tiết kiệm thời gian hiệu quả.
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
